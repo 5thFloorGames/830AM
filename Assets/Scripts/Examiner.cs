8c29c5837a63cf6629afd06e9bd8a451861ef574
Elegia <lauri.m.lukka@gmail.com>
2015-10-06 16:17:24 +0300

    Started lists

--- Assets/Scripts/Examiner.cs
+++ Assets/Scripts/Examiner.cs
@@ -1,33 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class Examiner : MonoBehaviour {
 
-	public GameObject TextR;
-	public Text description;
+	public GameObject TextExamine;
+	public List<string> CollectedAttributes = new List<string>();
+	// public string[] CollectedAttributes;
 
 	void Update() {
 		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 		RaycastHit hit;
 		if ((Physics.Raycast (ray, out hit, 6)) && hit.collider.tag == "Item") {
-			// print("you hit!");
-			// Debug.DrawLine(ray.origin, hit.point);
-			// print(hit.distance);
-			// print (hit.collider.tag);
+			// print(hit.distance) // print (hit.collider.tag);
 
-			TextR.SetActive(true);
+			TextExamine.SetActive (true);
 
-			if (TextR){  // and clicked
-				// print ("heureka!");
-				description.
+			if (TextExamine && Input.GetMouseButtonDown (0)) {
+
+				// ITEMI EI OLE VIELÄ TUTKITTU
+				if (hit.collider.gameObject.GetComponent<Information>().inspected == true) {
+					foreach (string s in hit.collider.gameObject.GetComponent<Information>().attributes) {
+						CollectedAttributes.Add (s);
+						print ("mitä jäi jäljel:" + hit.collider.gameObject.GetComponent<Information> ().attributes);
+						print ("here:" + CollectedAttributes);
+					}
+					hit.collider.gameObject.GetComponent<Information>().inspected = false;
+				}
+				// description.
 				// Text description = gameObject.AddComponent("Text");
 				// CREATES A HOVERING TEXT THAT WILL FADE IN 30s OVER OBJECT
+
+				// GOES THRU ALL element attributes and logs them into the variable
+				// foreach (hit.attribute){
+				//	print(hit.collider.);
 			}
-
-
-		} else {
-			TextR.SetActive(false);
+		}
+		else {
+			TextExamine.SetActive(false);
 		}
 	}
 }
--- Assets/Scripts/Information.cs
+++ Assets/Scripts/Information.cs
@@ -1,19 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Information : MonoBehaviour {
 	
 	public string Name;
-
-
-
-	void Start () {
+	public List<string> attributes;
+	public bool inspected = true;
 
 	
-	}
-	
-
-	void Update () {
-	
-	}
 }
